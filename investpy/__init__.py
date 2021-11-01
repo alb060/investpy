35be76bec9f0aaed07747a2b39095e052f3c78f7
Pawel Kazimierczyk <pawelkazimierczykk@gmail.com>
2021-11-01 13:00:06 +0100

    init

--- investpy/__init__.py
+++ investpy/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 import azure.functions as func
-#a
+#aa
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
